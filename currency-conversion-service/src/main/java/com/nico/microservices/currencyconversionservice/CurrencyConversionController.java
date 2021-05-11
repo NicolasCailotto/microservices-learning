c73a87fa7eb9b1319661a0dbdd89b658265ad881
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2021-05-10 22:34:48 -0300

    Agregado de Feign Client. TODO ver si es posible pasar a properties la config de FeignClient

--- currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package com.nico.microservices.currencyconversionservice;
 
+import com.nico.microservices.currencyconversionservice.feign.CurrencyExchangeServiceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,9 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     //where {from} and {to} represents the column
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
@@ -25,6 +30,13 @@ public class CurrencyConversionController {
                 .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
 
         CurrencyConversionBean response = responseEntity.getBody();
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    //where {from} and {to} represents the column
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }
---
+++ currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,12 @@
+package com.nico.microservices.currencyconversionservice.feign;
+
+import com.nico.microservices.currencyconversionservice.CurrencyConversionBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
+}
