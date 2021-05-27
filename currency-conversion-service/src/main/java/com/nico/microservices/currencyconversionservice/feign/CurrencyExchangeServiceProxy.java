221107171260a9db953ffbea2f1ec911cafc0b08
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2021-05-27 11:28:55 -0300

    se modifica la llamada al servicio para pasar por el gateway y se agregan comentarios

--- currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/feign/CurrencyExchangeServiceProxy.java
@@ -1,14 +1,20 @@
 package com.nico.microservices.currencyconversionservice.feign;
 
 import com.nico.microservices.currencyconversionservice.CurrencyConversionBean;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service"/*, url = "localhost:8000"*/)
-@RibbonClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server") // Se usa el gateway para llegar al servicio
+//@FeignClient(name = "currency-exchange-service"/*, url = "localhost:8000"*/) Annotation para usar directamente el servicio sin pasar por el gateway
+//@RibbonClient(name = "currency-exchange-service") Eureka ya provee Ribbon por su cuenta
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
-    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
+    // Se hace el mapeo al servicio pasando por el gateway
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+    /* Esto es para usar directamente el servicio sin el gateway
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);*/
+
 }
