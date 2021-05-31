6850dd5ceabdae0eb7f6ce8a75eb101dfdba13e2
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2021-05-31 11:10:55 -0300

    Add Swagger dummy config

---
+++ netflix-zull-api-gateway-server/src/main/java/com/nico/microservices/netflixzullapigatewayserver/config/DocumentationConfig.java
@@ -0,0 +1,31 @@
+package com.nico.microservices.netflixzullapigatewayserver.config;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary //Override Original swagger implementation with custom swagger resource provider.
+public class DocumentationConfig implements SwaggerResourcesProvider {
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("conversion-service", "/currency-conversion-service/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("exchange-service", "/currency-exchange-service/v2/api-docs", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+}
