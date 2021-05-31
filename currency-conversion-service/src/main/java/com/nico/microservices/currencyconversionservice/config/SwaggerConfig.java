fd81f95b6df4f5df6f05975432e4a2bf6a573ce4
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2021-05-31 11:11:39 -0300

    Add dummy swagger cofif

---
+++ currency-conversion-service/src/main/java/com/nico/microservices/currencyconversionservice/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.nico.microservices.currencyconversionservice.config;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+@SuppressWarnings("SpringJavaAutowiringInspection")
+public class SwaggerConfig {
+
+    private static final String API_KEY_NAME = "Access token";
+
+    private BuildProperties buildProperties;
+
+    public SwaggerConfig(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+                .apis(RequestHandlerSelectors.any()).paths(Predicates.not(PathSelectors.regex("/error.*"))).build()
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey(API_KEY_NAME, "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(buildProperties.get("api.info.security.include.pattern"))).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(new SecurityReference(API_KEY_NAME, authorizationScopes));
+    }
+}
